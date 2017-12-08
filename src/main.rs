b9364ce7b997c029748dad640d05e19b73a09791
Phil J. Laszkowicz <phil@fillip.pro>
2017-12-08 15:43:16 +0200

    Added faux parsing for command.

--- src/main.rs
+++ src/main.rs
@@ -13,32 +13,21 @@ of the cavernous walls to reflect the water running down, sporadically and seemi
 "
     );
 
+    let sleep_command: &str = "sleep";
     let mut s = String::new();
     print!("What would you like to do? ");
     let _ = stdout().flush();
 
-    stdin().read_line(&mut s).expect("You seem muddled and confused.");
+    stdin()
+        .read_line(&mut s)
+        .expect("You speak too softly."); 
 
-    if let Some('\n')=s.chars().next_back() {
-        s.pop();
-    }
-
-    if let Some('\r')=s.chars().next_back() {
-        s.pop();
-    }
-
-    println!("
+    match &s {
+        sleep_command => { 
+            println!("
 Sleep is a good plan. Now is not the time for action.
-");
-/*
-    let mut input_text = String::new();
-    io::stdin()
-        .read_line(&mut input_text)
-        .expect("failed to read from stdin");
-
-    let trimmed = input_text.trim();
-    match trimmed.parse::<u32>() {
-        Ok(i) => println!("your integer input: {}", i),
-        Err(..) => println!("this was not an integer: {}", trimmed)
-    };*/
+"
+            ); 
+        }
+    }
 }
